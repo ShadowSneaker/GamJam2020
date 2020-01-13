5e9f01e9b453b3d22f18e1da8c1056a7588ea559
ShadowSneaker <33228779+ShadowSneaker@users.noreply.github.com>
2020-01-13 16:19:19 +0000

    Big Boi Arror

--- GameJam2020/Assets/Scripts/Entities/Player/PlayerController.cs
+++ GameJam2020/Assets/Scripts/Entities/Player/PlayerController.cs
@@ -12,17 +12,38 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private float YeetStrength = 5.0f;
 
+    [Tooltip("The maximum distance to determine the maximu strength.")]
+    [SerializeField]
+    private float MaxDistance = 100.0f;
+
+    [Tooltip("The maximum thickness of the line.")]
+    [SerializeField]
+    private float THICC_ness = 2.0f;
+
+    [Tooltip("The starting colour for the line")]
+    [SerializeField]
+    private Color StartColour = Color.white;
+
+    [Tooltip("The ending colour for the line.")]
+    [SerializeField]
+    private Color EndColour = Color.red;
+
 
     public Rigidbody2D Head;
 
     // Determines if the head can be yeeted.
     private bool CanYeet;
 
+    // A reference to the attached line renderer.
+    private LineRenderer Line = null;
+
 
     public void Start()
     {
         Cursor.visible = ShowCursor;
         Cursor.lockState = (ShowCursor) ? CursorLockMode.Confined : CursorLockMode.Locked;
+
+        Line = GetComponent<LineRenderer>();
     }
 
 
@@ -47,6 +68,23 @@ public class PlayerController : MonoBehaviour
         if (!CanYeet && Head.IsSleeping())
         {
             CanYeet = true;
+
+
+        }
+
+        Line.enabled = CanYeet;
+
+        if (Line)
+        {
+            Line.SetPosition(0, Head.position);
+            Line.SetPosition(1, Camera.main.ScreenToWorldPoint(Input.mousePosition));
+
+            float Percent = (Vector2.Distance(Head.position, Camera.main.ScreenToWorldPoint(Input.mousePosition)) / MaxDistance);
+            Color LerpColor = Color.Lerp(StartColour, EndColour, Percent);
+            Line.startColor = LerpColor;
+            Line.endColor = LerpColor;
+
+            Line.endWidth = Mathf.Lerp(0.01f, THICC_ness, Percent);
         }
     }
 
