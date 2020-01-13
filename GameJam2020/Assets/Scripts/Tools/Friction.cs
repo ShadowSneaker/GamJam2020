2a6048540682186cdacef8822bd6bb843c570e46
ShadowSneaker <33228779+ShadowSneaker@users.noreply.github.com>
2020-01-13 15:08:51 +0000

    Friction Script

---
+++ GameJam2020/Assets/Scripts/Tools/Friction.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Friction : MonoBehaviour
+{
+    [Tooltip("Determiens how much angular drag should be applied to the head.")]
+    [SerializeField]
+    private float AngularDrag = 0.5f;
+
+    [Tooltip("Determines how much linear drag should be applied to the head.")]
+    [SerializeField]
+    private float LinearDrag = 0.3f;
+
+
+
+    private void OnCollisionEnter2D(Collision2D Other)
+    {
+        if (Other.gameObject.CompareTag("Player"))
+        {
+            Rigidbody2D Rigid = Other.gameObject.GetComponent<Rigidbody2D>();
+            if (Rigid)
+            {
+                Rigid.angularDrag = AngularDrag;
+                Rigid.drag = LinearDrag;
+            }
+        }
+    }
+
+
+    private void OnCollisionExit2D(Collision2D Other)
+    {
+        if (Other.gameObject.CompareTag("Player"))
+        {
+            Rigidbody2D Rigid = Other.gameObject.GetComponent<Rigidbody2D>();
+            if (Rigid)
+            {
+                Rigid.angularDrag = 0.01f;
+                Rigid.drag = 0.0f;
+            }
+        }
+    }
+}
