fd38e001b7150f44779d4bf468e6e9014db51ec9
ShadowSneaker <33228779+ShadowSneaker@users.noreply.github.com>
2020-01-13 15:11:18 +0000

    Basic Pause Script

---
+++ GameJam2020/Assets/Scripts/Tools/PauseScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PauseScript
+{
+    public static bool GamePaused = false;
+
+
+    public static void Pause()
+    {
+        Time.timeScale = 0.0f;
+        GamePaused = true;
+    }
+
+
+    public static void Unpause()
+    {
+        Time.timeScale = 1.0f;
+        GamePaused = false;
+    }
+
+
+    public static void TogglePause()
+    {
+        if (GamePaused)
+        {
+            Unpause();
+        }
+        else
+        {
+            Pause();
+        }
+    }
+}
