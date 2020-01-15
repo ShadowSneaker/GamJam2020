45edabe79a55af80a4bf0ce8bd30464a577a214b
Herobyron <33229975+Herobyron@users.noreply.github.com>
2020-01-15 11:25:25 +0000

    Nodes made and Pressure plate in work

---
+++ GameJam2020/Assets/Scripts/Traps/Iceicle.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Iceicle : MonoBehaviour
+{
+
+    // this is the iceicles rigid body
+    private Rigidbody2D RB;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RB = GetComponent<Rigidbody2D>();
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TestBelow();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            collision.GetComponent<HealthScript>().ApplyDamage(35.0f);
+        }
+    }
+
+    private void TestBelow()
+    {
+        RaycastHit2D hit;
+
+        hit = Physics2D.Raycast(gameObject.transform.position, Vector2.down, 100000.0f);
+
+        if(hit && hit.collider.CompareTag("Player"))
+        {
+           
+            RB.WakeUp();
+        }
+
+    }
+
+
+}
---
+++ GameJam2020/Assets/Scripts/Traps/PressurePlate.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlate : MonoBehaviour
+{
+    [Tooltip("all the objects that you want the pressure plate to rotate")]
+    [SerializeField]
+    private List<GameObject> RotateObjects = new List<GameObject>();
+
+    [Tooltip("How much you want all of the objects to be rotated by")]
+    [SerializeField]
+    private float RotationAmount = 0.0f;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    //a function to rotate the game object that is set in this script
+    public void RotateGameObject()
+    {
+
+        for(int i = 0; i < RotateObjects.Count; i++)
+        {
+            RotateObjects[i].transform.eulerAngles += new Vector3(RotateObjects[i].transform.rotation.x, RotateObjects[i].transform.rotation.y, (RotateObjects[i].transform.rotation.z + RotationAmount));
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+            RotateGameObject();
+    }
+
+}
