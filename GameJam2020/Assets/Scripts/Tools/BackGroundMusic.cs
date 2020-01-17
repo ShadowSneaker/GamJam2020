fb56e4f3e914c4670d3777e7b59534b0ab8e28cc
Herobyron <33229975+Herobyron@users.noreply.github.com>
2020-01-17 10:23:04 +0000

    Audio Changes

---
+++ GameJam2020/Assets/Scripts/Tools/BackGroundMusic.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGroundMusic : MonoBehaviour
+{
+    [Tooltip("The music that you want to be played whilst playing the game")]
+    [SerializeField]
+    private SoundScript BackgroundMusicScript = null;
+
+
+    [Tooltip("The Background Music prefab you dont wont destroyed")]
+    [SerializeField]
+    private GameObject SoundManager = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        if (!FindObjectOfType<BackGroundMusic>().CompareTag("MusicManager"))
+        {
+
+            LoadSounds();
+            DontDestroyOnLoad(SoundManager);
+            DontDestroyOnLoad(this);
+        }
+    }
+
+  
+    private void LoadSounds()
+    {
+        BackgroundMusicScript.SetAudio(GetComponent<AudioSource>());
+        BackgroundMusicScript.Play();
+
+        SoundManager = Instantiate(SoundManager);
+    }
+
+
+
+
+}
